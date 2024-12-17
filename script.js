0df35829d437a4df99754e7a017f58a01d4a0a0b
Bairdwolf <136029428+Bairdwolf@users.noreply.github.com>
2024-12-16 18:07:17 -0600

    Add highlight feature & new grid button

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //Function to draw grid with squares
 function drawGrid(sides){
     const container=document.querySelector(".container");
+    //now deletes anything in grid on startup
+    const rows=document.querySelectorAll(".row");
+    rows.forEach((row)=>{
+        container.removeChild(row);
+    });
     //Calculating gap size and adding it to make checkers
     const gapSize=480/(2*sides-1)
     container.style.gap=`${gapSize}px`;
@@ -14,6 +19,10 @@ function drawGrid(sides){
             const newBox=document.createElement("div");
             newBox.classList.add("box");
             newRow.appendChild(newBox);
+            //adds hover effect to change color
+            newBox.addEventListener("mouseover", (e) => {
+                newBox.style.backgroundColor="red";
+            });
         }
         container.appendChild(newRow);
     }
@@ -21,3 +30,14 @@ function drawGrid(sides){
 
 //draws default grid of 16
 drawGrid(16);
+
+//adds button to create new grid
+const button=document.querySelector("button");
+button.addEventListener("click", ()=>{
+    let input=prompt("Please enter number of sides");
+    //ensures it cant be more than 100 to save memory
+    if (input>100){
+        input=100;
+    }
+    drawGrid(input);
+});
